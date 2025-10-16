7bd663f6fcfaad8af3575163cd4aa2a7c7b8dba9
alex6damian <164188351+alex6damian@users.noreply.github.com>
2025-10-16 16:36:53 +0300

    update

--- RestaurantAutomation/RestaurantAutomation/settings.py
+++ RestaurantAutomation/RestaurantAutomation/settings.py
@@ -75,9 +75,15 @@ WSGI_APPLICATION = 'RestaurantAutomation.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "mydb"),
+        "USER": os.environ.get("POSTGRES_USER", "myuser"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "mypassword"),
+        "HOST": os.environ.get("DATABASE_HOST", "localhost"),
+        "PORT": os.environ.get("DATABASE_PORT", "5432"),
+        # recomandat pentru producție: connection pooling
+        "CONN_MAX_AGE": int(os.environ.get("CONN_MAX_AGE", 60)),
     }
 }
 
